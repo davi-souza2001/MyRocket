c048962db45a694e3bf7151174ba7dc059a85ce8
davi-souza2001 <davisouza2001dv@gmail.com>
2022-02-17 13:37:58 -0300

    remove tests because error

--- src/pages/App.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react'
-import Inicial from './index'
-
-test('sub', () => {
-    const { getByText } = render(<Inicial/>)
-
-    expect(getByText('Olaaa')).toBeTruthy()
-})
