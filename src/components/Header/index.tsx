a861fe1ca3b54e2c67351e3a3bf53f87ebfc7892
davi-souza2001 <davisouza2001dv@gmail.com>
2022-02-18 11:17:23 -0300

    start add toggle

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,26 @@
+import { useState } from 'react';
 import { HiMenu } from 'react-icons/hi';
-import { AiFillRocket } from 'react-icons/ai';
+import { AiFillRocket, AiOutlineClose } from 'react-icons/ai';
 import styles from './Header.module.css'
 
-export function Header(){
+export function Header() {
+    const [open, setOpen] = useState(false)
     return (
         <div className={styles.contentGeral}>
             <div className={styles.contentLogo}>
-                <AiFillRocket/>
+                <AiFillRocket />
                 <p>MyRocket</p>
             </div>
             <div className={styles.contentOptions}>
-                <HiMenu/>
+                {open ? (
+                    <AiOutlineClose onClick={() => setOpen(!open)} />
+                ) : (
+                    <HiMenu onClick={() => setOpen(!open)} />
+                )}
+                <div className={open ? styles.contentToggle : styles.contentToggleInicial }>
+                    <p>Opcao 1</p>
+                    <p>Opcao 2</p>
+                </div>
             </div>
         </div>
     )
