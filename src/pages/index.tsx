f56e473aaaf0d2f5748cc6268df2d4ce4e39aa3d
davi-souza2001 <davisouza2001dv@gmail.com>
2022-02-21 14:27:32 -0300

    start create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import { Header } from "../components/Header";
+import { HiOutlineGlobe } from "react-icons/hi";
+
+import styles from '../styles/Home.module.css'
 
 export default function index(){
   return (
     <div>
       <Header/>
+      <div className={styles.contentTitle}>
+        <div className={styles.contentIcon}>
+          <HiOutlineGlobe/>
+        </div>
+        <h2>Choosing a stack</h2>
+      </div>
     </div>
   )
 } 
