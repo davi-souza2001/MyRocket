bf155728ecb438efaa0f3e36fe6fd565d1731527
davi-souza2001 <davisouza2001dv@gmail.com>
2022-02-22 15:39:18 -0300

    start set images in box

--- src/components/BoxStacks/index.tsx
+++ src/components/BoxStacks/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import styles from './BoxStacks.module.css'
 
-type BoxStacks = {}
+import Front from '../../../public/areas/FrontIcon.svg'
+import Image from 'next/image'
+
+type BoxStacks = {
+    area: String,
+    description: String
+}
 
 export function BoxStacks(props: BoxStacks) {
     return (
         <div className={styles.contentGeral}>
             <div className={styles.contentBox}>
-                <h4>Front-End</h4>
-                <p>Comunidade Front-end, aqui você pode ver encontrar outros passageiros embarcando nas mesmas tecnologias que você. Entre elas, aqui as pessoas a bordo gostam de: React, Angular, Vue</p>
+                <h4>{props.area}</h4>
+                <p>{props.description}</p>
             </div>
-            <div>
-                <p>Imagem</p>
+            <div className={styles.contentImage}>
+                <Image src={Front} />
             </div>
         </div>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,9 +16,9 @@ export default function index(){
         <h2>Choosing a stack</h2>
       </div>
       <div className={styles.contentBoxes}>
-        <BoxStacks/>
-        <BoxStacks/>
-        <BoxStacks/>
+        <BoxStacks area={'Front-End'} description={'Comunidade Front-end, aqui você pode ver encontrar outros passageiros embarcando nas mesmas tecnologias que você. Entre elas, aqui as pessoas a bordo gostam de: React, Angular, Vue'}/>
+        <BoxStacks area={'Back-End'} description={'Comunidade Back-end, aqui você pode ver encontrar outros passageiros embarcando nas mesmas tecnologias que você. Entre elas, aqui as pessoas a bordo gostam de: Node, Java'}/>
+        <BoxStacks area={'Mobile'} description={'Comunidade Mobile, aqui você pode ver encontrar outros passageiros embarcando nas mesmas tecnologias que você. Entre elas, aqui as pessoas a bordo gostam de: React-Native, Kotlin'}/>
       </div>
     </>
   )
