4c76424c8bb1f9f4e63425c79f0f6102cc473fde
davi-souza2001 <davisouza2001dv@gmail.com>
2022-02-25 14:49:09 -0300

    set verify comum exists

--- src/pages/communities.tsx
+++ src/pages/communities.tsx
@@ -1,9 +1,17 @@
+import Router from 'next/router'
+import { useEffect } from 'react'
 import useComum from '../service/hook/useComum'
 import styles from '../styles/Communities.module.css'
 
 
 export default function Comum() {
   const { comumUnic } = useComum()
+
+  useEffect(() => {
+    if(comumUnic === 'Null'){
+      Router.replace('/')
+    }
+  },[])
 
   return (
     <div className={styles.contentGeral}>
