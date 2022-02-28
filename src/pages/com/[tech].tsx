c3140ae2f26660d13c7cddccadb045a321d641e2
davi-souza2001 <davisouza2001dv@gmail.com>
2022-02-28 20:55:44 -0300

    start boxes comum

--- src/pages/com/[tech].tsx
+++ src/pages/com/[tech].tsx
@@ -1,13 +1,32 @@
 import { useRouter } from "next/router";
-
+import { HiOutlineGlobe } from "react-icons/hi";
+import { Header } from "../../components/Header";
+import styles from '../../styles/Com.module.css'
 
 export default function Commun() {
   const router = useRouter();
-  const comumSeach = router.query.tech;
+  const comumSearch = router.query.tech;
 
   return (
     <>
-      <div style={{color: '#fff'}}>{comumSeach}</div>
+      <Header />
+      <div className={styles.contentGeral}>
+        <div className={styles.title}>
+          <h3>Planeta {comumSearch}</h3>
+          <HiOutlineGlobe />
+        </div>
+        <div className={styles.contetBoxes}>
+          <div className={styles.contentLeft}>
+            Test
+          </div>
+          <div className={styles.contentCenter}>
+            Test
+          </div>
+          <div className={styles.contentRight}>
+            Test
+          </div>
+        </div>
+      </div>
     </>
   )
 }
