0af87c15baba7c8dbbae8c2164fa3df4b47bf23d
davi-souza2001 <davisouza2001dv@gmail.com>
2022-02-28 23:35:42 -0300

    finish box members

--- src/components/MembersComum/index.tsx
+++ src/components/MembersComum/index.tsx
@@ -4,7 +4,27 @@ import styles from './MembersComum.module.css'
 export function MembersComum() {
     return (
         <div className={styles.contentGeral}>
-            MembersComum
+            <div className={styles.contentTitle}>
+                <p>Comandante</p>
+            </div>
+            <div className={styles.contentCommander}>
+                <p>Davi Souza</p>
+            </div>
+            <div className={styles.contentTitle}>
+                <p>Tripulantes</p>
+            </div>
+            <div className={styles.contentMembers}>
+                <p>Davi</p>
+                <p>Davi</p>
+                <p>Davi</p>
+                <p>Davi</p>
+                <p>Davi</p>
+                <p>Davi</p>
+                <p>Davi</p>
+                <p>Davi</p>
+                <p>Davi</p>
+                <p>Davi</p>
+            </div>
         </div>
     )
 }
