3222fa4f91ac998589eb64de9bbaa800017d2fd8
davi-souza2001 <davisouza2001dv@gmail.com>
2022-03-01 09:18:51 -0300

    up

--- src/components/PostsMoreLiked/index.tsx
+++ src/components/PostsMoreLiked/index.tsx
@@ -11,21 +11,21 @@ export default function PostsMoreLiked() {
         </div>
         <div className={styles.contentPosts}>
             <div className={styles.boxPost}>
-                <Image src={Teste} width={30} height={30}/>
+                <Image src={Teste} width={30} height={30} alt="User image"/>
                 <div className={styles.contentNameandPost}>
                     <p>Blablablabalbal</p>
                     <p>Davi Souza</p>
                 </div>
             </div>
             <div className={styles.boxPost}>
-                <Image src={Teste} width={30} height={30}/>
+                <Image src={Teste} width={30} height={30} alt="User image"/>
                 <div className={styles.contentNameandPost}>
                     <p>Blablablabalbal</p>
                     <p>Davi Souza</p>
                 </div>
             </div>
             <div className={styles.boxPost}>
-                <Image src={Teste} width={30} height={30}/>
+                <Image src={Teste} width={30} height={30} alt="User image"/>
                 <div className={styles.contentNameandPost}>
                     <p>Blablablabalbal</p>
                     <p>Davi Souza</p>
