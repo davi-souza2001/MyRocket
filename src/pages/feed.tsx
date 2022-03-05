6ef2546dcd638ca7f689b249260e1834c7b3ca53
davi-souza2001 <davisouza2001dv@gmail.com>
2022-03-05 09:06:47 -0300

    start page feed

---
+++ src/pages/feed.tsx
@@ -0,0 +1,28 @@
+import { BoxPostComum } from "../components/BoxPostComum";
+import { Header } from "../components/Header";
+import styles from '../styles/Feed.module.css'
+
+export default function Feed() {
+  return (
+    <>
+        <Header/>
+        <div className={styles.contentTitle}>
+            <h2>Posts recentes da Comunidade </h2>
+        </div>
+        <div className={styles.contentTechs}>
+            <div className={styles.techBox}>
+                <p>React</p>
+            </div>
+            <div className={styles.techBox}>
+                <p>Node</p>
+            </div>
+            <div className={styles.techBox}>
+                <p>Java</p>
+            </div>
+            <div>
+                <BoxPostComum/>
+            </div>
+        </div>
+    </>
+  )
+}
