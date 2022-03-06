dea5a6ecf2d951b331d046bf200afacad0a088e3
davi-souza2001 <davisouza2001dv@gmail.com>
2022-03-06 19:46:54 -0300

    page feed finish

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -3,26 +3,30 @@ import { Header } from "../components/Header";
 import styles from '../styles/Feed.module.css'
 
 export default function Feed() {
-  return (
-    <>
-        <Header/>
-        <div className={styles.contentTitle}>
-            <h2>Posts recentes da Comunidade </h2>
-        </div>
-        <div className={styles.contentTechs}>
-            <div className={styles.techBox}>
-                <p>React</p>
+    return (
+        <>
+            <Header />
+            <div className={styles.contentTitle}>
+                <h2>Posts recentes da Comunidade </h2>
             </div>
-            <div className={styles.techBox}>
-                <p>Node</p>
+            <div className={styles.contentTechs}>
+                <div className={styles.techBox}>
+                    <p>React</p>
+                </div>
+                <div className={styles.techBox}>
+                    <p>Node</p>
+                </div>
+                <div className={styles.techBox}>
+                    <p>Java</p>
+                </div>
             </div>
-            <div className={styles.techBox}>
-                <p>Java</p>
+            <div className={styles.contentPosts}>
+                <BoxPostComum />
+                <BoxPostComum />
+                <BoxPostComum />
+                <BoxPostComum />
+                <BoxPostComum />
             </div>
-            <div>
-                <BoxPostComum/>
-            </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
