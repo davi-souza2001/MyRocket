9f2863e8aaea75c57432a88fe313a4b6688a8881
davi-souza2001 <davisouza2001dv@gmail.com>
2022-03-06 20:16:32 -0300

    start page search

---
+++ src/pages/search.tsx
@@ -0,0 +1,19 @@
+import { Header } from "../components/Header";
+import styles from '../styles/Search.module.css'
+
+export default function Search() {
+    return (
+        <>
+            <Header />
+            <div className={styles.contentTitle}>
+                <h2>Escolha qual modalidade você quer procurar outros devs !</h2>
+            </div>
+            <div className={styles.contentSearch}>
+                <form>
+                    <input type="text" />
+                    <button type="submit">Search</button>
+                </form>
+            </div>
+        </>
+    )
+}
