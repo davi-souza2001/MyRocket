d85f76fa41b3a34e1b3e84c529b9f95e841c5b78
davi-souza2001 <davisouza2001dv@gmail.com>
2022-03-12 15:51:20 -0300

    add bar in profile page

--- src/pages/profile/[user].tsx
+++ src/pages/profile/[user].tsx
@@ -4,7 +4,7 @@ import { Header } from "../../components/Header";
 
 import styles from '../../styles/Profile.module.css'
 
-import Test from '../../../public/backgroundGalaxy.svg'
+import Test from '../../../public/img/social_medias/gmail.svg'
 
 export default function Profile() {
   const router = useRouter();
@@ -13,7 +13,20 @@ export default function Profile() {
   return (
     <>
         <Header/>
-        <div className={styles.contentImageBackGround}>
+        <div className={styles.contentImageBackGround}/>
+        <div className={styles.contentImageUser}>
+          <div className={styles.imageUser}>
+            <Image src={Test} width={60} height={60}/>
+          </div>
+        </div>
+        <div className={styles.contentUserInfo}>
+          <h2>Davi Souza</h2>
+          <p>@{comumSearch}</p>
+        </div>
+        <div className={styles.contentUserDescription}>
+          <div className={styles.contentDescBox}>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quis harum nesciunt. Nulla dignissimos sed omnis repellat doloribus, sunt dolorem reiciendis impedit, facilis enim modi necessitatibus ab explicabo architecto adipisci!</p>
+          </div>
         </div>
     </>
   )
