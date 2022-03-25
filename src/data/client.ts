110317e9ff92acdd39eabde8955eda110e3ece20
davi-souza2001 <davisouza2001dv@gmail.com>
2022-03-24 21:21:55 -0300

    update production

--- src/data/client.ts
+++ src/data/client.ts
@@ -2,8 +2,12 @@ import axios from "axios";
 
 // Url base que o axios vai consumir em toda a aplicação
 
+// const api = axios.create({
+//     baseURL: "http://localhost:5000"
+// });
+
 const api = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "https://myrocketdev.herokuapp.com/"
 });
 
 export default api;
