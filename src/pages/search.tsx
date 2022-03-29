ec03f42de86038c9d7fef6d03d41c1ab1428f27c
davi-souza2001 <davisouza2001dv@gmail.com>
2022-03-29 12:26:34 -0300

    start functon search by comum

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,20 +1,73 @@
+import { useState } from "react";
+
+import Client from '../data/client'
+
 import { BoxUser } from "../components/BoxUser";
 import { Header } from "../components/Header";
+
 import styles from '../styles/Search.module.css'
 
 export default function Search() {
+    const [howSearch, setHowSearch] = useState('NickName')
+    const [search, setSearch] = useState('')
+    const [foundUsers, setFoundUsers] = useState([])
+
+    console.log(foundUsers)
+
+    function handleChangeHowToSearch() {
+        if (howSearch === 'NickName') {
+            setHowSearch('Community')
+            setSearch('')
+        }
+        if (howSearch === 'Community') {
+            setHowSearch('NickName')
+            setSearch('')
+        }
+    }
+
+    async function handleFoundUsersByComum(e: any) {
+        e.preventDefault()
+        const sendData = {
+            comum: search
+        }
+        const comum = { search }
+        try {
+            const data = await Client.post('/users/searchuserByComum', sendData).then((res) => {
+                setFoundUsers(res.data.userFoundComum)
+                return res.data
+            })
+        } catch (error: any) {
+            console.log(error.response.data)
+        }
+    }
+
     return (
         <>
             <Header />
             <div className={styles.contentTitle}>
                 <h2>Choose which modality you want to find other devs!</h2>
             </div>
+            <div className={styles.contentSelectHowToSearch}>
+                <button onClick={handleChangeHowToSearch}>{howSearch}</button>
+            </div>
             <div className={styles.contentSearch}>
-                <form>
-                    <input type="text" />
-                    <button type="submit">Search</button>
-                </form>
+                {howSearch === 'NickName' ? (
+                    <form onSubmit={handleFoundUsersByComum}>
+                        <input type="text" onChange={(e) => setSearch(e.target.value)} value={search} />
+                        <button type="submit">Search</button>
+                    </form>
+                ) : <h1>alo</h1>}
+
                 <div className={styles.contentResponseUser}>
+                    <div className={styles.contentBoxUser}>
+                        <BoxUser />
+                    </div>
+                    <div className={styles.contentBoxUser}>
+                        <BoxUser />
+                    </div>
+                    <div className={styles.contentBoxUser}>
+                        <BoxUser />
+                    </div>
                     <div className={styles.contentBoxUser}>
                         <BoxUser />
                     </div>
