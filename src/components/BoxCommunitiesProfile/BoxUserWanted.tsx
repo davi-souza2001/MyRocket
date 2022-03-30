e8a3e5d2d7dff3c33d66e73f97a627be5d8d0203
davi-souza2001 <davisouza2001dv@gmail.com>
2022-03-30 15:26:33 -0300

    upgrade profiles search

---
+++ src/components/BoxCommunitiesProfile/BoxUserWanted.tsx
@@ -0,0 +1,30 @@
+import route from "next/router";
+
+import { HiOutlineGlobe } from "react-icons/hi";
+
+import styles from './BoxCommunitiesProfile.module.css'
+
+interface BoxUserWantedProps{
+    comumone?: String;
+    comumtwo?: String;
+    comumthree?: String;
+}
+
+export function BoxUserWanted(props: BoxUserWantedProps) {
+
+  return (
+    <div className={styles.contentGeral}>
+      <div className={styles.communities}>
+        <div className={styles.community}>
+          <HiOutlineGlobe className={styles.worldIcon}/><p onClick={() => route.push(`/com/${props?.comumone}`)}>{props?.comumone}</p>
+        </div>
+        <div className={styles.community}>
+          <HiOutlineGlobe className={styles.worldIcon}/><p onClick={() => route.push(`/com/${props?.comumtwo}`)}>{props?.comumtwo}</p>
+        </div>
+        <div className={styles.community}>
+          <HiOutlineGlobe className={styles.worldIcon}/><p onClick={() => route.push(`/com/${props?.comumthree}`)}>{props?.comumthree}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/profile/[user].tsx
+++ src/pages/profile/[user].tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import route, { useRouter } from "next/router";
 import Image from "next/image";
 import { HiCog } from "react-icons/hi";
 
-import UseAuth from "../../service/hook/useAuth";
+import Client from '../../data/client';
 
 import { Header } from "../../components/Header";
-import { BoxCommunitiesProfile } from "../../components/BoxCommunitiesProfile";
+import { BoxUserWanted } from "../../components/BoxCommunitiesProfile/BoxUserWanted";
 import { BoxProjectsProfile } from "../../components/BoxProjectsProfile";
 import { BoxSocialMediaProfile } from "../../components/BoxSocialMediaProfile";
 
@@ -14,15 +14,54 @@ import styles from '../../styles/Profile.module.css'
 
 import Test from '../../../public/img/social_medias/gmail.svg'
 
-export default function Profile() {
-  const { user, logout } = UseAuth()
+interface User {
+  name?: String;
+  nickname?: String;
+  seniority?: String;
+  area?: String;
+  comumone?: String;
+  comumtwo?: String;
+  comumthree?: String;
+  description?: String;
+  linkedin?: String;
+  github?: String;
+  youtube?: String;
+  instagram?: String;
+  photo?: String;
+  email?: String;
+  gas?: Number;
+}
 
+export default function Profile() {
+  const [user, setUser] = useState<User>({})
   const [communities, setCommunities] = useState(true)
   const [projects, setProjects] = useState(false)
   const [socialMedia, setSocialMedia] = useState(false)
 
   const router = useRouter();
   const userSearch = router.query.user;
+
+  async function handleFoundUsersByNickName() {
+    const sendData = {
+      usersearch: userSearch
+    }
+
+    if (userSearch) {
+      try {
+        const data = await Client.post('/users/searchuserByNickName', sendData).then((res) => {
+          setUser(res.data)
+          console.log(res.data)
+          return res.data
+        })
+      } catch (error: any) {
+        console.log(error.response.data.error)
+      }
+    }
+  }
+
+  useEffect(() => {
+    handleFoundUsersByNickName()
+  }, [userSearch])
 
   function handleComum() {
     setCommunities(true)
@@ -46,44 +85,54 @@ export default function Profile() {
     <>
       <Header />
       <div className={styles.contentImageBackGround} />
-      <div className={styles.contentImageUser}>
-        <div className={styles.imageUser} onClick={logout}>
-          <Image src={user?.photo || Test} width={60} height={60} alt="logo" />
-        </div>
-        <div className={styles.imageUserDesktop} onClick={logout}>
-          <Image src={user?.photo || Test} width={100} height={100} alt="logo" />
-        </div>
-      </div>
-      <div className={styles.contentUserInfo}>
-        <h2>{user?.name} <HiCog style={{ 'cursor': 'pointer' }} onClick={() => route.push('/editProfile')} /></h2>
-        <p>@{user?.nickname}</p>
-      </div>
-      <div className={styles.contentUserDescription}>
-        <div className={styles.contentDescBox}>
-          <p>{user?.description}</p>
-        </div>
-      </div>
-      <div className={styles.contentBarOptions}>
-        <p onClick={handleComum}>Communities</p>
-        <p onClick={handleProjects}>Projects</p>
-        <p onClick={handleSocialMedia}>Social media</p>
-      </div>
-      <div className={styles.contentOptionSelected}>
-        {communities && (
-          <BoxCommunitiesProfile />
-        )}
-        {projects && (
-          <BoxProjectsProfile />
-        )}
-        {socialMedia && (
-          <BoxSocialMediaProfile />
-        )}
-      </div>
-      <div className={styles.contentOptionUserDesktop}>
-        <BoxCommunitiesProfile />
-        <BoxProjectsProfile />
-        <BoxSocialMediaProfile />
-      </div>
+      {user && (
+        <>
+          <div className={styles.contentImageUser}>
+            <div className={styles.imageUser}>
+              <Image src={user?.photo || Test} width={60} height={60} alt="logo" />
+            </div>
+            <div className={styles.imageUserDesktop}>
+              <Image src={user?.photo || Test} width={100} height={100} alt="logo" />
+            </div>
+          </div>
+          <div className={styles.contentUserInfo}>
+            <h2>{user?.name} <HiCog style={{ 'cursor': 'pointer' }} onClick={() => route.push('/editProfile')} /></h2>
+            <p>@{user?.nickname}</p>
+          </div>
+          <div className={styles.contentUserDescription}>
+            <div className={styles.contentDescBox}>
+              <p>{user?.description}</p>
+            </div>
+          </div>
+          <div className={styles.contentBarOptions}>
+            <p onClick={handleComum}>Communities</p>
+            <p onClick={handleProjects}>Projects</p>
+            <p onClick={handleSocialMedia}>Social media</p>
+          </div>
+          <div className={styles.contentOptionSelected}>
+            {communities && (
+              <BoxUserWanted
+                comumone={user.comumone}
+                comumtwo={user.comumtwo}
+                comumthree={user.comumthree} />
+            )}
+            {projects && (
+              <BoxProjectsProfile />
+            )}
+            {socialMedia && (
+              <BoxSocialMediaProfile />
+            )}
+          </div>
+          <div className={styles.contentOptionUserDesktop}>
+            <BoxUserWanted
+              comumone={user.comumone}
+              comumtwo={user.comumtwo}
+              comumthree={user.comumthree} />
+            <BoxProjectsProfile />
+            <BoxSocialMediaProfile />
+          </div>
+        </>
+      )}
     </>
   )
 }
