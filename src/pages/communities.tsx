54befde4b243f1d376dd2bd8bb9a69d14a856e2a
davi-souza2001 <davisouza2001dv@gmail.com>
2022-06-07 21:40:50 -0300

    config editor

--- src/pages/communities.tsx
+++ src/pages/communities.tsx
@@ -31,16 +31,16 @@ export default function Comum() {
   const { comumUnic } = useComum();
 
   useEffect(() => {
-    if(comumUnic === 'Null'){
+    if (comumUnic === 'Null') {
       Router.replace('/');
     }
-  },[comumUnic])
+  }, [comumUnic])
 
   return (
     <div className={styles.contentGeral}>
-      <Header/>
+      <Header />
       <div className={styles.contentTitle}>
-        <BiWrench/>
+        <BiWrench />
         <div className={styles.title}>
           <p>Which</p>
           <p className={styles.communityTitle}>{comumUnic}</p>
@@ -50,28 +50,28 @@ export default function Comum() {
       <div className={styles.contentBoxesTechs}>
         {comumUnic === 'Front' && (
           <>
-            <BoxTechs img={react} title={'React'} onClick={() => Router.push('/com/React')}/>
-            <BoxTechs img={next} title={'Next'} onClick={() => Router.push('/com/Next')}/>
-            <BoxTechs img={angular} title={'Angular'} onClick={() => Router.push('/com/Angular')}/>
-            <BoxTechs img={vue} title={'Vue'} onClick={() => Router.push('/com/Vue')}/>
-            <BoxTechs img={ember} title={'Ember'} onClick={() => Router.push('/com/Ember')}/>
+            <BoxTechs img={react} title={'React'} onClick={() => Router.push('/com/React')} />
+            <BoxTechs img={next} title={'Next'} onClick={() => Router.push('/com/Next')} />
+            <BoxTechs img={angular} title={'Angular'} onClick={() => Router.push('/com/Angular')} />
+            <BoxTechs img={vue} title={'Vue'} onClick={() => Router.push('/com/Vue')} />
+            <BoxTechs img={ember} title={'Ember'} onClick={() => Router.push('/com/Ember')} />
           </>
         )}
         {comumUnic === 'Back' && (
           <>
-            <BoxTechs img={node} title={'Node'} onClick={() => Router.push('/com/Node')}/>
-            <BoxTechs img={java} title={'Java'} onClick={() => Router.push('/com/Java')}/>
-            <BoxTechs img={firebase} title={'Firebase'} onClick={() => Router.push('/com/Firebase')}/>
-            <BoxTechs img={mysql} title={'Sql'} onClick={() => Router.push('/com/Sql')}/>
-            <BoxTechs img={mongo} title={'Mongo'} onClick={() => Router.push('/com/Mongo')}/>
+            <BoxTechs img={node} title={'Node'} onClick={() => Router.push('/com/Node')} />
+            <BoxTechs img={java} title={'Java'} onClick={() => Router.push('/com/Java')} />
+            <BoxTechs img={firebase} title={'Firebase'} onClick={() => Router.push('/com/Firebase')} />
+            <BoxTechs img={mysql} title={'Sql'} onClick={() => Router.push('/com/Sql')} />
+            <BoxTechs img={mongo} title={'Mongo'} onClick={() => Router.push('/com/Mongo')} />
           </>
         )}
         {comumUnic === 'Mobile' && (
           <>
-            <BoxTechs img={flutter} title={'Flutter'} onClick={() => Router.push('/com/Flutter')}/>
-            <BoxTechs img={react} title={'React-Native'} onClick={() => Router.push('/com/React-Native')}/>
-            <BoxTechs img={ionic} title={'Ionic'} onClick={() => Router.push('/com/Ionic')}/>
-            <BoxTechs img={swift} title={'Swift'} onClick={() => Router.push('/com/Swift')}/>
+            <BoxTechs img={flutter} title={'Flutter'} onClick={() => Router.push('/com/Flutter')} />
+            <BoxTechs img={react} title={'React-Native'} onClick={() => Router.push('/com/React-Native')} />
+            <BoxTechs img={ionic} title={'Ionic'} onClick={() => Router.push('/com/Ionic')} />
+            <BoxTechs img={swift} title={'Swift'} onClick={() => Router.push('/com/Swift')} />
           </>
         )}
       </div>
