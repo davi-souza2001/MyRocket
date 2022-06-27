6af80bbe6c4e3c2f9012f6950006756dd049e133
davi-souza2001 <davisouza2001dv@gmail.com>
2022-06-27 15:24:42 -0300

    new modal input

--- src/components/SendPost/index.tsx
+++ src/components/SendPost/index.tsx
@@ -16,15 +16,6 @@ import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
 const style = {
-	position: 'absolute' as 'absolute',
-	top: '50%',
-	left: '50%',
-	transform: 'translate(-50%, -50%)',
-	width: 400,
-	bgcolor: 'background.paper',
-	border: '2px solid #000',
-	boxShadow: 24,
-	p: 4,
 };
 
 interface SendPostProps {
@@ -68,21 +59,19 @@ export function SendPost(props: SendPostProps) {
 
 	return (
 		<div className={styles.contentGeral}>
-			{/* <Modal
+			<Modal
 				open={open}
 				onClose={handleClose}
 				aria-labelledby="modal-modal-title"
 				aria-describedby="modal-modal-description"
 			>
-				<Box sx={style}>
-					<Typography id="modal-modal-title" variant="h6" component="h2">
-						Text in a modal
-					</Typography>
-					<Typography id="modal-modal-description" sx={{ mt: 2 }}>
-						Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-					</Typography>
-				</Box>
-			</Modal> */}
+				<div className={styles.boxModalContentGeral}>
+					<strong>Discovery as text</strong>
+					<div>
+						<p>input</p>
+					</div>
+				</div>
+			</Modal>
 			<div className={styles.contentImagemAndDescription}>
 				<div className={styles.contentImageUser}>
 					<Image src={user?.avatar || Test} width={40} height={40} alt="Logo user" />
@@ -92,17 +81,20 @@ export function SendPost(props: SendPostProps) {
 			<div className={styles.contentBar} />
 			<div className={styles.contentOptionsPost}>
 				<HiPencil
+					onClick={handleOpen}
 					style={{
 						cursor: 'pointer'
 					}}
 				/>
 				<HiCamera
+					onClick={handleOpen}
 					style={{
 						cursor: 'pointer',
 						margin: '0px 60px'
 					}}
 				/>
 				<HiDocument
+					onClick={handleOpen}
 					style={{
 						cursor: 'pointer'
 					}}
