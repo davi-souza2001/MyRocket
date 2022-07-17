63f9df712faa5948be180f3e8c97a8f654a7ab8d
davi-souza2001 <davisouza2001dv@gmail.com>
2022-07-17 11:34:00 -0300

    env url

--- src/data/client.ts
+++ src/data/client.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const api = axios.create({
-	baseURL: "http://localhost:3333"
-});
-
 // const api = axios.create({
-//     baseURL: "https://apimyrocket-production.up.railway.app/"
+// 	baseURL: "http://localhost:3333"
 // });
+
+const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_URL_DATABASE
+});
 
 export default api;
